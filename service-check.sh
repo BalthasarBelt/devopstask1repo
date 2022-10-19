ecf38c27ffd02b44d954395bd0a2e63afd251dc2
BalthasarBelt <balthasarbeltthesupreme@gmail.com>
2022-10-19 19:05:55 +0200

    service-check.sh script 1.0

---
+++ service-check.sh
@@ -0,0 +1,12 @@
+#i/bin/bash
+#Script written by Martyn 19/10/2022
+#Purpose of script = check status of ssh, firewalld, docker statuses. Create log.
+
+services=(sshd firewalld docker)
+for i in "${services[@]}"
+do
+  if systemctl is-active --quiet $i && msg="$i service is running" ; then
+    systemctl is-enabled --quiet $i && msg="${msg} and is enabled"
+  fi
+  echo $msg | tee -a service-checklog
+done
